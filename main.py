51b679fb661da07226a3a24d0ca1462b98c0813a
Aaron Keech <aaronkeech@gatech.edu>
2019-06-28 01:29:19 -0400

    Change backward execution order to topological sort with DFS

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import math
+from collections import defaultdict
 
 class UnaryBackward:
     def __init__(self, a):
         self.a = a
-        self.a.grad_fn_count += 1
     def update(self, a_grad):
         self.a.accumulate_gradient(a_grad)
-        self.a.backward()
     def __call__(self, gradient):
         raise NotImplementedError
 
@@ -14,13 +13,9 @@ class BinaryBackward:
     def __init__(self, a, b):
         self.a = a
         self.b = b
-        self.a.grad_fn_count += 1
-        self.b.grad_fn_count += 1
     def update(self, a_grad, b_grad):
         self.a.accumulate_gradient(a_grad)
         self.b.accumulate_gradient(b_grad)
-        self.a.backward()
-        self.b.backward()
     def __call__(self, gradient):
         raise NotImplementedError
 
@@ -30,12 +25,16 @@ class AddBackward(BinaryBackward):
         da = gradient
         db = gradient
         self.update(da, db)
+    def __str__(self):
+        return '<AddBackward>'
 
 def add(a, b):
     if type(b) == float or type(b) == int:
         b = Number(b)
     ret = Number(a.value + b.value)
     ret.grad_fn = AddBackward(a, b)
+    ret.parent_nodes.append(a)
+    ret.parent_nodes.append(b)
     return ret
 
 
@@ -44,12 +43,16 @@ class MulBackward(BinaryBackward):
         da = gradient * self.b.value
         db = gradient * self.a.value
         self.update(da, db)
+    def __str__(self):
+        return '<MulBackward>'
 
 def mul(a, b):
     if type(b) == float or type(b) == int:
         b = Number(b)
     ret = Number(a.value * b.value)
     ret.grad_fn = MulBackward(a, b)
+    ret.parent_nodes.append(a)
+    ret.parent_nodes.append(b)
     return ret
 
 
@@ -62,12 +65,16 @@ class DivBackward(BinaryBackward):
         db = (-1 * (b**2)) * a * gradient
 
         self.update(da, db)
+    def __str__(self):
+        return '<DivBackward>'
 
 def div(a, b):
     if type(b) == float or type(b) == int:
         b = Number(b)
     ret = Number(a.value / b.value)
     ret.grad_fn = DivBackward(a, b)
+    ret.parent_nodes.append(a)
+    ret.parent_nodes.append(b)
     return ret
 
 class SigmoidBackward(UnaryBackward):
@@ -75,11 +82,14 @@ class SigmoidBackward(UnaryBackward):
         value = 1 / (1 + math.exp(-self.a.value))
         da = (value * (1 - value)) * gradient
         self.update(da)
+    def __str__(self):
+        return '<SigmoidBackward>'
 
 def sigmoid(a):
     value = 1 / (1 + math.exp(-a.value))
     ret = Number(value)
     ret.grad_fn = SigmoidBackward(a)
+    ret.parent_nodes.append(a)
     return ret
 
 
@@ -87,20 +97,22 @@ class ReluBackward(UnaryBackward):
     def __call__(self, gradient):
         da = gradient if a.value > 0 else 0
         self.update(da)
+    def __str__(self):
+        return '<ReluBackward>'
 
 def relu(a):
     ret = Number(a.value if a.value > 0 else 0)
     ret.grad_fn = ReluBackward(a)
+    ret.parent_nodes.append(a)
     return ret
 
 
-# TODO: Need to keep track of parents, and do a  topological sort to remove the need for grad_fn_count
 class Number(object):
     def __init__(self, value, grad=None):
         self.value = value
         self.grad = grad
         self.grad_fn = None
-        self.grad_fn_count = 0
+        self.parent_nodes = []
     def __repr__(self):
         return str(self)
     def __str__(self):
@@ -115,19 +127,66 @@ class Number(object):
         self.grad += grad
 
     def _backward(self):
-        self.grad_fn(self.grad)
+        if self.grad_fn is not None:
+            self.grad_fn(self.grad)
 
     # HACK: This delays backproping from this node until all other dependencies are done
     def backward(self, gradient=None):
-        # If we backprop from the last output
         if gradient is None and self.grad is None:
             self.grad = 1
-            self.grad_fn_count += 1
+        else:
+            self.grad = gradient
 
-        if self.grad_fn is not None:
-            self.grad_fn_count -= 1
-            if self.grad_fn_count == 0:
-                self._backward()
+        for node in self.build_graph():
+            node._backward()
+
+    # Return a dictionary of child->parent edges
+    def get_edges(self):
+        forward_edges = defaultdict(set)
+        backward_edges = defaultdict(set)
+        for n in self.parent_nodes:
+            forward_edges[n].add(self)
+            backward_edges[self].add(n)
+
+            # Merge forward and backward dicts
+            new_forward, new_backward = n.get_edges()
+            for n in new_forward:
+                forward_edges[n] = forward_edges[n].union(new_forward[n])
+            for n in new_backward:
+                backward_edges[n] = backward_edges[n].union(new_backward[n])
+        return forward_edges, backward_edges
+
+    # Return the first edge found, any will work
+    def _get_first_edge(self, edges):
+        if len(edges) > 0:
+            node = list(list(edges.keys()))[0]
+            return node
+        return None
+
+    # Run DFS until we reach a leaf node
+    def _dfs(self, edges):
+        e = self._get_first_edge(edges)
+        while e is not None and len(edges[e]) > 0:
+            e = list(edges[e])[0]
+        return e
+
+    def build_graph(self):
+        forward_edges, backward_edges = self.get_edges()
+        forward_edges[self] = set()
+
+        # Follow forward edges with DFS, and use backward edges to remove from forward edges
+        sorted_list = []
+
+        e = self._dfs(forward_edges)
+        while e is not None:
+            sorted_list.append(e)
+            for b in backward_edges[e]:
+                forward_edges[b].remove(e)
+
+            del forward_edges[e]
+            e = self._dfs(forward_edges)
+
+        return sorted_list
 
     def __radd__(self, other):
         return self.__add__(other)
